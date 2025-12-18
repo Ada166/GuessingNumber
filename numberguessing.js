59e88ee6aaea28c489ee935449f571176fe340b0
Ada <xandrewsteam@gmail.com>
2025-12-18 21:57:02 +0100

    Add html file and function for guessNumber

---
+++ numberguessing.js
@@ -0,0 +1,12 @@
+function guessNumber() {
+    let secretNumber = Math.floor(Math.random() * 10) + 1;
+    let guess = prompt("Guess a number between 1 and 10: ");
+
+    if (guess == secretNumber) {
+        console.log("You won, congratulation") 
+        } else{
+            console.log("Wrong, it was " + secretNumber + "try again!")
+        }
+    }
+
+    guessNumber();
